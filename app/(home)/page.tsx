b992bd4cce32410615a1004f4d13633ae79c4b32
Joao Pedro <joao.fpsilva98@gmail.com>
2024-02-26 19:03:20 -0300

    feat: add search button component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,19 +1,25 @@
 import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
-import Image from "next/image"
-import Header from "../_components/header"
-export default function Home(){
-return  <div>
-    <Header />
+import Image from "next/image";
+import Header from "../_components/header";
+import Search from "./_compoents/search";
+export default function Home() {
+  return (
+    <div>
+      <Header />
 
-    
-    <div className="px-5 pt-5">
-                        <h2 className="text-xl font-bold"> Hello João</h2>
-                        <p className="capitalize text-sm">
-                            {format (new Date(), "EEEE',' dd' de' MMMM ",{
-                                locale: ptBR,
-                            })}
-                        </p>
-                    </div>
-</div>
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold"> Hello João</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE',' dd' de' MMMM ", {
+            locale: ptBR,
+          })}
+        </p>
+      </div>
+
+     <div className="px-5 mt-6">
+     <Search />
+     </div>
+    </div>
+  );
 }
