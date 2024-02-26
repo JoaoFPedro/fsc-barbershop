7e59987df90b01c64dda5469d2c206911a2b9c6c
Joao Pedro <joao.fpsilva98@gmail.com>
2024-02-26 18:16:52 -0300

    feat: welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,19 @@
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
 import Image from "next/image"
 import Header from "../_components/header"
 export default function Home(){
 return  <div>
     <Header />
+
+    
+    <div className="px-5 pt-5">
+                        <h2 className="text-xl font-bold"> Hello João</h2>
+                        <p className="capitalize text-sm">
+                            {format (new Date(), "EEEE',' dd' de' MMMM ",{
+                                locale: ptBR,
+                            })}
+                        </p>
+                    </div>
 </div>
 }
--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -4,11 +4,12 @@ import { Card, CardContent } from "./ui/card";
 import Image from "next/image";
 
 
+
 const Header = () => {
     return ( 
         <Card>
             <CardContent className="p-5 justify-between items-center flex flex-row">
-                <Image src="/logo.png" alt={"FSW Barber"} height={22} width={120}/>
+                <Image src="/logo_jpbarber02.png" alt={"FSW Barber"} height={22} width={120}/>
                 <Button variant="outline" size="icon" className="h-8 w-8">
 
                     <MenuIcon size={16}/>
