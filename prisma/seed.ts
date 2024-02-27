627d4be3b9ef5562bbb29208cf1912ebd5515594
Joao Pedro <joao.fpsilva98@gmail.com>
2024-02-27 20:24:15 -0300

    feat: add imageurl to service

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -125,6 +125,7 @@ async function seedDatabase() {
               connect: {
                 id: barbershop.id,
               },
+              imageUrl: service.imageUrl,
             },
           },
         });
