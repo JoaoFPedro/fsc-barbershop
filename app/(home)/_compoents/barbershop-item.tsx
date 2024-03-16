e78543a3b750fc6917aceb294fec4d551267c11c
Joao Pedro <joao.fpsilva98@gmail.com>
2024-03-16 16:35:49 -0300

    feat: ensure user can search for a barbershop

--- app/(home)/_compoents/barbershop-item.tsx
+++ app/(home)/_compoents/barbershop-item.tsx
@@ -23,7 +23,7 @@ const BarberShopItem = ({ barbershop }: BarberShopItemProps) => {
 
 
   return (
-    <Card className="min-w-[167px] max-w-[167px] rounded-2xl">
+    <Card className="min-w-full max-w-full rounded-2xl">
       <CardContent className="px-1 py-0">
         <div className="w-full relative h-[159px]">
            <div className="absolute top-2 left-2 z-50">
---
+++ app/barbershops/page.tsx
@@ -0,0 +1,42 @@
+import BarberShopItem from "../(home)/_compoents/barbershop-item";
+import Header from "../_components/header";
+import { db } from "../_lib/prisma";
+
+interface BarbershopsPage {
+    searchParams: {
+        search?: string
+    }
+}
+
+const BarberShopsPage = async ({searchParams}: BarbershopsPage) => {
+    const barbershops = await db.barbershop.findMany({
+        where:{
+            name:{
+                contains: searchParams.search,
+                mode: "insensitive"
+            },
+        },
+    })
+
+    return ( 
+        <>
+        <Header/>
+        <div className="px-5 py-6">
+
+        <h1 className="text-gray-400 font-bold text-xs uppercase">Resultados para &quat;{searchParams.search}&quat;</h1>
+        <div className="grid grid-cols-2 mt-3 gap-4">
+            {barbershops.map((barbershop) =>(
+                <div key={barbershop.id} className="w-full"> 
+                <BarberShopItem  barbershop={barbershop}/>
+                </div>
+            ))}
+        </div>
+        </div>
+
+
+
+        </>
+     );
+}
+ 
+export default BarberShopsPage;
