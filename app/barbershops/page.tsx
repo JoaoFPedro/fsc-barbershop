48b3e8247102b74118f052ed2aa24011d6d29b3e
Joao Pedro <joao.fpsilva98@gmail.com>
2024-03-16 16:43:03 -0300

    fix: change the url when changing the search at search bar

--- app/barbershops/page.tsx
+++ app/barbershops/page.tsx
@@ -3,6 +3,7 @@ import { redirect } from "next/navigation";
 import BarberShopItem from "../(home)/_compoents/barbershop-item";
 import Header from "../_components/header";
 import { db } from "../_lib/prisma";
+import Search from "../(home)/_compoents/search";
 
 interface BarbershopsPage {
     searchParams: {
@@ -27,9 +28,14 @@ const BarberShopsPage = async ({searchParams}: BarbershopsPage) => {
     return ( 
         <>
         <Header/>
-        <div className="px-5 py-6">
 
-        <h1 className="text-gray-400 font-bold text-xs uppercase">Resultados para &quat;{searchParams.search}&quat;</h1>
+        
+        
+        <div className="px-5 py-6 flex flex-col gap-6">
+
+        <Search/>
+
+        <h1 className="text-gray-400 font-bold text-xs uppercase">Resultados para: {searchParams.search}</h1>
         <div className="grid grid-cols-2 mt-3 gap-4">
             {barbershops.map((barbershop) =>(
                 <div key={barbershop.id} className="w-full"> 
